f81adc802e43ba098305cae2510354cfdfbf846e
vsht-code <vsht.code@gmail.com>
2023-07-12 23:46:44 +0300

    added link counter and bitlink checker

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
+from urllib.parse import urlparse
 import requests
 
 
+api_token = '29da32489aa26561114e4ecd78af19700f50028c'
 
-# '29da32489aa26561114e4ecd78af19700f50028c'
 
-headers = {
-    'Authorization': 'Bearer 29da32489aa26561114e4ecd78af19700f50028c'
-}
+url = input()
 
-long_url = input()
 
-payload = {
-    'long_url': long_url,
-}
+def shorten_link(token, url):
+    response = requests.post(
+        'https://api-ssl.bitly.com/v4/bitlinks', 
+        headers={'Authorization': 'Bearer ' + token},
+        json={'long_url': url})
+    response.raise_for_status()
+    return response.json()['link']
 
-api_url = 'https://api-ssl.bitly.com/v4/bitlinks'
 
-response = requests.post(api_url, headers=headers, json=payload)
-response.raise_for_status()
+def count_clicks(token, url):
+    response = requests.get(
+        'https://api-ssl.bitly.com/v4/bitlinks/{0}{1}/clicks/summary'
+        .format(url.netloc, url.path),
+        headers={'Authorization': 'Bearer ' + token},)
+    response.raise_for_status()
+    return response.json()['total_clicks']
 
-print(response.json()['link'])
+
+def is_bitlink(token, url):
+    response = requests.get(
+        'https://api-ssl.bitly.com/v4/bitlinks/{0}{1}'
+        .format(url.netloc, url.path),
+        headers={'Authorization': 'Bearer ' + token},)
+    return response.ok
+
+parsed_bitlink = urlparse(url)
+
+
+try:
+    if is_bitlink(api_token, parsed_bitlink):
+        total_clicks = count_clicks(api_token, parsed_bitlink)
+        print(total_clicks)
+    else:
+        bitlink = shorten_link(api_token, url)
+        print(bitlink)
+except requests.exceptions.HTTPError:
+    exit('Вы ввели неверную ссылку!')
