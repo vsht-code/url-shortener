5842c0e35b1a636eccaee7fe2bf32098530fbcde
vsht-code <vsht.code@gmail.com>
2023-07-20 11:25:00 +0300

    added arparser

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from urllib.parse import urlparse
 from dotenv import load_dotenv
 import requests
 import os
+import argparse
 
 
 load_dotenv()
@@ -9,12 +10,9 @@ load_dotenv()
 api_token = os.getenv("TOKEN")
 
 
-url = input()
-
-
 def shorten_link(token, url):
     response = requests.post(
-        'https://api-ssl.bitly.com/v4/bitlinks', 
+        'https://api-ssl.bitly.com/v4/bitlinks',
         headers={'Authorization': 'Bearer ' + token},
         json={'long_url': url})
     response.raise_for_status()
@@ -36,6 +34,15 @@ def is_bitlink(token, url):
         .format(url.netloc, url.path),
         headers={'Authorization': 'Bearer ' + token},)
     return response.ok
+
+
+parser = argparse.ArgumentParser(
+    description='Программа для сокращения ссылок '
+    'или подсчёта количества переходов по bitlink')
+
+parser.add_argument('url', help='Введите ссылку или bitlink:')
+args = parser.parse_args()
+url = args.url
 
 parsed_bitlink = urlparse(url)
 
